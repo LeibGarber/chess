50f153bea913f57bc9ed6d42d8e547561d292176
leibGarber <leibGarber@gmail.com>
2019-02-17 21:47:50 +0200

    built board with no pieces

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const boardSize = 8;
+const pieces = [
+  ["R", "P", "", "", "", "", "p", "r"],
+  ["N", "P", "", "", "", "", "p", "n"],
+  ["B", "P", "", "", "", "", "p", "b"],
+  ["K", "P", "", "", "", "", "p", "k"],
+  ["Q", "P", "", "", "", "", "p", "q"],
+  ["B", "P", "", "", "", "", "p", "b"],
+  ["N", "P", "", "", "", "", "p", "n"],
+  ["R", "P", "", "", "", "", "p", "r"]
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+          <h2>This shit is chess not checkers</h2>
+          <h3>-Omar</h3>
+        </div>
+        <div className="board">
+          {Array(boardSize)
+            .fill(0)
+            .map((_, rowIndex) => (
+              <div className="row" key={rowIndex}>
+                {Array(boardSize)
+                  .fill(0)
+                  .map((__, colIndex) => (
+                    <div className="square" key={colIndex}>
+                      {pieces[colIndex][rowIndex]}&nbsp;
+                    </div>
+                  ))}
+              </div>
+            ))}
+        </div>
       </div>
     );
   }
